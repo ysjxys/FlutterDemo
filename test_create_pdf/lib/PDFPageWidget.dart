cec7c7e98c3447e32e7a6d8f8737d408a530514b
yushengjie <ysjxys@163.com>
2020-04-11 20:50:42 +0800

    update pdf demo

--- test_create_pdf/lib/PDFPageWidget.dart
+++ test_create_pdf/lib/PDFPageWidget.dart
@@ -7,8 +7,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 import 'package:pdf_viewer_plugin/pdf_viewer_plugin.dart';
-import 'package:printing/printing.dart';
-import 'dart:ui';
 import 'package:image/image.dart' as im;
 import 'package:flutter/services.dart' show rootBundle;
 
@@ -19,57 +17,70 @@ class PDFPageWidget extends StatefulWidget {
 
 class _PDFPageWidgetState extends State<PDFPageWidget> {
   String path;
-  final imageProvider = AssetImage('assets/images/add_blue.png');
 
   @override
   void initState() {
     super.initState();
   }
 
-  Future<File> getImageFileFromAssets(String path) async {
-
-    final byteData = await rootBundle.load('assets/$path');
+  Future<PdfImage> getImageFileFromAssets(
+      pw.Document pdfDoc, String path) async {
+    final byteData = await rootBundle.load('$path');
 
     print('byteData: $byteData');
-    final file = File('${(await getApplicationSupportDirectory()).path}/$path');
-    await file.writeAsBytes(byteData.buffer
+    File file =
+        File('${(await getApplicationSupportDirectory()).path}/tempFile');
+    file = await file.writeAsBytes(byteData.buffer
         .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    im.Image imImage = im.decodeImage(file.readAsBytesSync());
 
-    return file;
+    return PdfImage(
+      pdfDoc.document,
+      image: imImage.data.buffer.asUint8List(),
+      width: imImage.width,
+      height: imImage.height,
+    );
   }
 
   void createAndSavePdf() async {
-    final pdf = pw.Document();
+    final pdf = new pw.Document();
+
+    final image =
+        await getImageFileFromAssets(pdf, 'assets/images/add_blue.png');
+    final image2 = await getImageFileFromAssets(
+        pdf, 'assets/images/home_adverting_default.png');
 
 
-    final byteData = await rootBundle.load('assets/images/add_blue.png');
+    print('PdfColor: ${PdfColor.fromRYB(0.5, 0.5, 0.5).toHex()}');
 
-    print('byteData: $byteData');
-    File file = File('${(await getApplicationSupportDirectory()).path}/$path');
-    file = await file.writeAsBytes(byteData.buffer
-        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
-
-
-    final img = im.decodeImage(file.readAsBytesSync());
-
-    final image = PdfImage(
-      pdf.document,
-      image: img.data.buffer.asUint8List(),
-      width: img.width,
-      height: img.height,
-    );
-
-    pdf.addPage(pw.Page(
+    pdf.addPage(
+      pw.Page(
         pageFormat: PdfPageFormat.a4,
         build: (pw.Context context) {
           return pw.Container(
               child: pw.Center(
                   child: pw.Column(children: [
             pw.Text("Hello World", style: pw.TextStyle(fontSize: 18)),
-            pw.Text("Hello World", style: pw.TextStyle(fontSize: 18)),
-            pw.Image(image),
+            pw.Text("来个中文", style: pw.TextStyle(fontSize: 18)),
+
+            pw.Container(
+              width: 200,
+              height: 200,
+
+//              color: PdfColor.fromRYB(0.5, 0.5, 0.5),
+              color: PdfColor.fromHex('#ff9b7449'),
+//              child: pw.Image(image2),
+            ),
+                    pw.Image(image),//, width: 100, height: 200
+//            pw.Container(
+//                height: 500,
+//                width: 200,
+//                color: PdfColor.fromRYB(0.5, 0.5, 0.5)),
           ])));
-        })); // Page
+        },
+      ),
+    );
+    pdf.addPage(pw.Page());
 
     final output = await getApplicationSupportDirectory();
     final filePdf = File("${output.path}/example.pdf");
@@ -109,9 +120,13 @@ class _PDFPageWidgetState extends State<PDFPageWidget> {
                   createAndSavePdf();
                 },
               ),
-              RaisedButton(child: Text('share pdf'), onPressed: () {}),
+              RaisedButton(
+                  child: Text('share pdf'),
+                  onPressed: () {
+                    print('share pdf select');
+                  }),
               Image.asset('assets/images/add_blue.png'),
-              Image(image: imageProvider),
+              Image(image: AssetImage('assets/images/add_blue.png')),
             ],
           ),
         ),
